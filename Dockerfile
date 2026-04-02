d14f7d1dff158e051119f493b5462fbb05021089
seyha yoeun <yoeunseyha11@gmail.com>
2026-04-02 17:54:51 +0700

    Cofigure DockerFile and docker-compose.yml

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use Node.js 18
+FROM node:18
+
+# Set working directory
+WORKDIR /app
+
+# Copy package files first
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the app
+COPY . .
+
+# Expose port
+EXPOSE 3000
+
+# Run NestJS in dev mode
+CMD ["npm", "run", "start:dev"]
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import cookieParser from 'cookie-parser';
+import * as crypto from 'crypto';
+(global as any).crypto = crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
