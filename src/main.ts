4265f479e9a2ac7ad395a1c2ae7f7885453c29bf
seyha yoeun <yoeunseyha11@gmail.com>
2026-04-24 23:40:51 +0700

    Fix Cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ['http://localhost:3002', 'http://localhost:3001'],
+        origin: ['http://localhost:3002', 'http://localhost:3001','https://ys-bookstore.vercel.app'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         credentials: true,
     });
